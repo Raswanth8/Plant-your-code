a3e3d4aea7e260f9009b974cf316e023dd7985b6
Raswanth8 <raswanth8901@gmail.com>
2021-12-03 19:57:56 +0530

    Level 1

---
+++ Level 1/Ch2.js
@@ -0,0 +1,42 @@
+/**
+ * The following represents all your plants' locations 
+ * in your garden and whether they are or are not currently 
+ * getting sun.
+ * 
+ * const plant_1 = {
+ *      gardenLocation: [1, 1],
+ *      needsWater: true
+ * };
+ * 
+ * const plant_2 = {
+ *      gardenLocation: [1, 2],
+ *      needsWater: false
+ * };
+ * 
+ * const plant_3 = {
+ *      gardenLocation: [2, 1],
+ *      needsWater: false
+ * };
+ * 
+ * const plant_4 = {
+ *      gardenLocation: [2, 2],
+ *      needsWater: true
+ * };
+ * 
+ * const plants = [plant_1, plant_2, plant_3, plant_4];
+*/
+
+/**
+ * Write a function that takes in your array of plants and 
+ * returns a new array of garden locations you should water.
+ *
+ * @param {array} plants - Your array of plants.
+ * @return {array} - An array of garden locations you should 
+ * water.
+ */
+
+ function whereToWater(plantsArray) {
+    return plantsArray
+        .filter(plant => plant.needsWater)
+        .map(plant => plant.gardenLocation)
+}
